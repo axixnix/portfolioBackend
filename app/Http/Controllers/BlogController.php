b341d5ae4deb17225625de98f8504c06e97d17c5
B <henryirons@yahoo.com>
2021-07-30 04:38:20 +0100

    CRUD for blogs done

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -10,19 +10,40 @@ class BlogController extends Controller
 {
     //
 
-    public function getBlog(){
+    public function getBlog($id){
+       $blog = Blog::find($id);
 
+       if($blog){
+           return response()->json(['blog'=>$blog],200);
+       }
+       return response()->json(['message'=>'blog with matching id not found'],404);
     }
+
+
     public function getBlogs(){
 
         $blogs = Blog::all();
-        return response()->json($blogs);
+        return response()->json(['blogs'=>$blogs],200);
 
     }
-    public function updateBlog(){
+    public function updateBlog(Request $request,$id){
+        $blog = Blog::find($id);
+        if($blog){
+            $blog->title=$request->title;
+            $blog->content=$request->content;
+            $blog->update();
+            return response()->json(['message'=>'blog updated successfully'],200);
+        }
+        return response()->json(['message'=>'no blog with matching id found'],404);
 
     }
-    public function deleteBlog(){
+    public function deleteBlog($id){
+     $blog = Blog::find($id);
+     if($blog){
+         $blog->delete();
+         return response()->json(['message'=>'blog deleted successfully'],200);
 
+     }
+     return response()->json(['message'=>'no blog with matching id found'],404);
     }
 }
