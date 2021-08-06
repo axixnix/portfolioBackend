ddfbf4ac917153a7d0f3152f1c9a6e6c76ca72d4
Eric McWinNEr <eapriok@gmail.com>
2021-08-06 10:51:36 +0100

    Updated the confirmation email

--- resources/views/mails/verify.blade.php
+++ resources/views/mails/verify.blade.php
@@ -2,8 +2,10 @@
 <html>
     <body>
         <p>Hello {{ $user->first_name }},</p>
-        <p>Your registration to the admin section of the portfolio admin was successful. Use the code below to verify your email.</p>
-        <p style="font-size: 36px">{{ $verification->payload }}</p>
+        <p>Your registration to the admin section of the portfolio admin was successful.
+{{--            Use the code below to verify your email.--}}
+        </p>
+{{--        <p style="font-size: 36px">{{ $verification->payload }}</p>--}}
         <p>Thanks!</p>
     </body>
 </html>
